c00f16b0874503023c8cf2a712392c9f395f700d
Andreina <rei.acee@gmail.com>
2024-06-13 12:00:20 -0400

    Completed Part 2

--- index.js
+++ index.js
@@ -12,3 +12,41 @@ try {
 } catch (error) {
     console.log(`Stack overflow occurred at count:`, counter)
 }
+
+//Part 2
+// write a recursive function that completely flattens the array
+function flattenArray(arr, result = []) {
+    arr.forEach(item => {
+        if (Array.isArray(item)) {
+            counter++
+            return () => flattenArray(item, result)
+        } else {
+            result.push(item)
+        }
+    })
+    return result
+}
+
+//Trampoline function to handle recusrion even though its not needed
+function trampoline(fn) {
+    return function (...args) {
+        let result = fn(...args)
+        while (typeof result === 'function') {
+            result = result()
+        }
+        return result
+    }
+}
+
+// Now a tramp for the recursive func
+const trampolinedFlatten = trampoline(flattenArray)
+
+//Testing the function
+const nestedArray = [1, [2, [3, 4], 5], 6, [7, [8, 9, [10, 11]]]]
+
+//flatten the array using trasmp recursive func
+const flattenedArray = trampolinedFlatten(nestedArray)
+console.log(flattenedArray) // [1, 2, 3, 4,
+
+
+
